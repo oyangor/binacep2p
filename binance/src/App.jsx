1e8f270e0d1e23fbae89876095ea5bca16fbfb17
oyangor <robertanthonyoyango@gmail.com>
2024-10-18 19:44:45 +0300

    first commit

---
+++ binance/src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css';
+
+import Binancep2p from '../components/14.binancep2p'
+
+function App() {
+
+
+  return (
+    <>
+
+      <Binancep2p />
+
+    </>
+  )
+}
+
+export default App
